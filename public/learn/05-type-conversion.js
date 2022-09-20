266844abbc02e5f436524b81e33907ef9d083c98
yamoo9 <yamoo9@naver.com>
2022-09-20 14:29:13 +0900

    learn. ES 타입 변환
    - string → number
    - number → string

--- public/learn/05-type-conversion.js
+++ public/learn/05-type-conversion.js
@@ -1,7 +1,40 @@
+/* -------------------------------------------------------------------------- */
+/* DOM 접근/조작                                                                  */
+/* -------------------------------------------------------------------------- */
+
+// 문서에서 .ButtonGroup 요소를 찾아 변수에 참조
+const buttonGroup = document.querySelector('.ButtonGroup');
+const target = document.querySelector('[data-target]');
+
+const STEP = 1;
+
+let initialTargetFontSize =
+  getComputedStyle(target).getPropertyValue('font-size');
+let currentTargetFontSize = parseFloat(initialTargetFontSize, 10);
+
+// 찾은 요소 노드의 하위에 요소 노드에 이벤트 전파(propagation) ← 이벤트 위임
+// 옵셔널 체이닝 (optional chaining)
+buttonGroup?.addEventListener('click', function (event) {
+  const buttonElement = event.target;
+  let buttonLabel = buttonElement.getAttribute('aria-label');
+  switch (buttonLabel) {
+    case '글자 크기 증가':
+      console.log('increment');
+      currentTargetFontSize += STEP;
+      break;
+    case '글자 크기 감소':
+      console.log('decrement');
+      currentTargetFontSize -= STEP;
+      break;
+  }
+
+  console.log(currentTargetFontSize);
+  target.style.fontSize = currentTargetFontSize + 'px';
+});
+
 /* ---------------------------------------------------------------------- */
 /* Type Conversion                                                        */
 /* ---------------------------------------------------------------------- */
-
 
 /* 데이터 → 문자 ----------------------------------------------------------- */
 
@@ -10,7 +43,6 @@
 // undefined, null
 
 // boolean
-
 
 /* 데이터 → 숫자 ----------------------------------------------------------- */
 
@@ -24,8 +56,7 @@
 
 // numeric string
 
-
 /* 데이터 → 불리언 ---------------------------------------------------------- */
 
 // null, undefined, 0, NaN, ''
-// 위에 나열한 것 이외의 것들 
+// 위에 나열한 것 이외의 것들
