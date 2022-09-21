cb0915ff8cb6dc0b1dad97d589d49e8ddd926834
yamoo9 <yamoo9@naver.com>
2022-09-21 10:02:38 +0900

    learn. 전개 연산자

--- client/src/ecmascript/chapter/05-rest-parameters.js
+++ client/src/ecmascript/chapter/05-rest-parameters.js
@@ -8,7 +8,7 @@
 
 var multiplyCount = function () {
   var first = arguments[0]; // 첫번째 매개변수
-  var rest = [].slice.call(arguments, 1); // 나머지 매개변수
+  var rest = Array.prototype.slice.call(arguments, 1); // 나머지 매개변수
 
   // for문 → Array.prototype.reduce 메서드 구문으로 변경해보세요.
   for (var i = 0, l = rest.length; i < l; ++i) {
@@ -18,3 +18,11 @@ var multiplyCount = function () {
 
   return first;
 };
+
+const multipleNumbers = (first, ...rest) =>
+  rest.reduce(
+    (acc, cur, index) => (index === 0 ? acc + cur : acc * cur),
+    first
+  );
+
+console.log(multipleNumbers(20, 3020, 10203, 200, 123));
