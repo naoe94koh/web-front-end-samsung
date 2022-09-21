33657d39df5d1e5754e3f627f276dc3277fa63b0
yamoo9 <yamoo9@naver.com>
2022-09-22 08:58:55 +0900

    learn. React 컴포넌트 vs React 요소

--- public/js/react.js
+++ public/js/react.js
@@ -2,26 +2,63 @@
 /* React + ReactDOM                                                           */
 /* -------------------------------------------------------------------------- */
 
+// HTMLDivElement  vs. <div></div>
+// React Component vs. React Element
+
+// React Component Type
+// React 요소를 재사용
+// - class (stateful): Container Component
+// - function (stateless): Presentational Component
+
 {
-  const { createElement: h } = React;
+  const { createElement: h, Component, StrictMode } = React;
 
-  const state = Object.freeze({
-    headline: 'React 라이브러리',
-    features: ['선언형 프로그래밍', '가상 DOM을 사용해 UI 업데이트'],
-  });
+  // Headline 컴포넌트 → `headline` React 요소를 생성
+  // React 함수 컴포넌트
+  // 네이밍 컨벤션 (파스칼 케이스)
+  function Headline(props) {
+    return h('h2', null, props.children);
+  }
 
-  const headline = h('h2', null, state.headline);
+  // List 컴포넌트 → `list` React 요소를 생성
+  const List = (props) => {
+    return h('ul', {
+      children: props.features.map((feature, index) => {
+        // ListItem 컴포넌트 → listItem 요소 생성
+        return h(ListItem, { key: index }, feature);
+      }),
+    });
+  };
 
-  const list = h('ul', {
-    children: state.features.map((feature, index) =>
-      h('li', { key: index }, feature)
-    ),
-  });
+  // ListItem 컴포넌트 → `listItem` React 요소를 생성
+  const ListItem = (props) => {
+    return h('li', null, props.children);
+  };
 
-  const app = h('div', { className: 'app' }, headline, list);
+  // App 컴포넌트 → `app` React 요소를 생성
+  // 컴포넌트 상태(state) 관리 (캡슐화)
+  class App extends Component {
+    constructor(props) {
+      super(props);
+      // 불변 상태 객체
+      this.state = {
+        headline: 'React 라이브러리',
+        features: ['선언형 프로그래밍', '가상 DOM을 사용해 UI 업데이트'],
+      };
+    }
+
+    render() {
+      return h(
+        'div',
+        { className: 'app' },
+        h(Headline, null, this.state.headline),
+        h(List, { features: this.state.features })
+      );
+    }
+  }
 
   const container = document.getElementById('react-root');
 
   const reactDomRoot = ReactDOM.createRoot(container);
-  reactDomRoot.render(app);
+  reactDomRoot.render(h(StrictMode, null, h(App)));
 }
